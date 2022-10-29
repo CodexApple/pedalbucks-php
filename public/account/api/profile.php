a21f107ddd2ea932ca11fe785ec11378640602ed
CodexApple | Ghostly | MilkyTune <41129289+CodexApple@users.noreply.github.com>
2022-10-29 13:49:06 +0800

    Added support for UUID Profile
    Added API Support for UUID Support

--- public/account/api/profile.php
+++ public/account/api/profile.php
@@ -7,6 +7,25 @@ $rawData = file_get_contents("php://input");
 $response = json_decode($rawData);
 
 switch ($_SERVER['REQUEST_METHOD']) {
+    case "GET":
+        if (isset($_GET['uuid']) && $profileDetails = $profile->getData($_GET['uuid'])) {
+            echo json_encode(
+                array(
+                    $profileDetails
+                ),
+                JSON_PRETTY_PRINT
+            );
+        } else {
+            echo json_encode(
+                array(
+                    "status" => "Error",
+                    "message" => "Invalid UUID"
+                ),
+                JSON_PRETTY_PRINT
+            );
+        }
+
+        break;
     case "POST":
         if ($profile->saveData($response->uuid, $response->firstname, $response->lastname)) {
             echo json_encode(
